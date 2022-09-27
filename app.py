a0873856eb8e36346ff8fa1692fcea7282a5bd88
gabribn99 <gabribn99@gmail.com>
2022-09-27 17:48:33 +0100

    Added all files

---
+++ app.py
@@ -0,0 +1,37 @@
+from nextcord import Intents
+from nextcord.ext import commands
+import requests
+import json
+import random
+import os
+
+links = json.load(open("gifs.json"))
+
+intents = Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='defcon ', intents=intents)
+
+
+@bot.command(name="saluda")
+async def SendMessage(ctx):
+    await ctx.send('¡Hola!, soy Bad Def, y estoy aquí para ayudarte.')
+
+
+@bot.command(name='perro')
+async def sendRandomDog(ctx):
+    response = requests.get("http://dog.ceo/api/breeds/image/random")
+    image_link = response.json()["message"]
+    await ctx.send(image_link)
+
+
+@bot.command(name="gifs", aliases=["perrocomiendo", "perrojugando", "perrodurmiendo"])
+async def Gifs(ctx):
+    await ctx.send(random.choice(links[ctx.invoked_with]))
+
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as: {bot.user.name}")
+
+if __name__ == '__main__':
+    bot.run(os.environ["DISCORD_TOKEN"])
